289eaf2f82cc478496ebf28595549bb51beb4821
Anthony Whipple <whipple@email.unc.edu>
2022-03-16 15:33:24 -0400

    Change cfget to confget for new OS support

--- images2api.sh
+++ images2api.sh
@@ -7,28 +7,28 @@ if pidof -o %PPID -x "images2api.sh">/dev/null; then
 fi
 
 # API_KEY is no longer necessary
-#API_KEY=`cfget --cfg /home/pi/bin/config.ini dataHandler/API_KEY`
+#API_KEY=`confget -f /home/pi/bin/config.ini API_KEY`
 #if [[ -z "$API_KEY" ]]
 #then
 #    echo $'Did not find config value API_KEY.  Exiting\n'
 #    exit 1
 #fi
 
-API_USER=`cfget --cfg /home/pi/bin/config.ini dataHandler/API_USER`
+API_USER=`confget -f /home/pi/bin/config.ini API_USER`
 if [[ -z "$API_USER" ]]
 then
     echo $'Did not find config value API_USER.  Exiting\n'
     exit 1
 fi
 
-API_PASS=`cfget --cfg /home/pi/bin/config.ini dataHandler/API_PASS`
+API_PASS=`confget -f /home/pi/bin/config.ini API_PASS`
 if [[ -z "$API_PASS" ]]
 then
     echo $'Did not find config value API_PASS.  Exiting\n'
     exit 1
 fi
 
-CAMERA_ID=`cfget --cfg /home/pi/bin/config.ini dataHandler/CAMERA_ID`
+CAMERA_ID=`confget -f /home/pi/bin/config.ini CAMERA_ID`
 if [[ -z "$CAMERA_ID" ]]
 then
     echo $'Did not find config value CAMERA_ID.  Exiting\n'
