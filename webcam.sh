9be1ca8d763d5c8d50f9c1bd09d06f39022edf06
Anthony Whipple <whipple@email.unc.edu>
2022-03-17 11:47:53 -0400

    Support new camera library for R Pi Bullseye

--- webcam.sh
+++ webcam.sh
@@ -11,5 +11,11 @@ cd "$PIC_DIR$TODAY"
 
 HM=$(date +"_%H%M")
 
-raspistill -o $PIC_DIR$TODAY/$TODAY$HM.jpg -a 12 -a "%Y-%m-%d %X %Z" -ae 128 -q 30 -rot 270 -ex verylong -t 20000
+# For older versions of R Pi OS up to Buster
+#raspistill -o $PIC_DIR$TODAY/$TODAY$HM.jpg -a 12 -a "%Y-%m-%d %X %Z" -ae 128 -q 30 -rot 270 -ex verylong -t 20000
+
+# For R Pi OS Bullseye and beyond
+libcamera-still -o $PIC_DIR$TODAY/$TODAY$HM.jpg --metering average --exposure long -q 30 -t 20000 --post-process-file /home/pi/bin/drc.json
+mogrify -rotate 270 $PIC_DIR$TODAY/$TODAY$HM.jpg
+mogrify -pointsize 100 -fill white -undercolor '#00000080' -gravity North -annotate +0+5 "`date`" $PIC_DIR$TODAY/$TODAY$HM.jpg
 
