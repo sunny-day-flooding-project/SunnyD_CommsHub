f87c57eaf7f70a63acbfb5df241e2b043d5644cf
Anthony Whipple <whipple@email.unc.edu>
2022-08-17 11:24:04 -0400

    Test whether libcamera-still exists and use if so.

--- webcam.sh
+++ webcam.sh
@@ -11,11 +11,12 @@ cd "$PIC_DIR$TODAY"
 
 HM=$(date +"_%H%M")
 
-# For older versions of R Pi OS up to Buster
-#raspistill -o $PIC_DIR$TODAY/$TODAY$HM.jpg -a 12 -a "%Y-%m-%d %X %Z" -ae 128 -q 30 -rot 270 -ex verylong -t 20000
-
-# For R Pi OS Bullseye and beyond
-timeout -s 2 60s libcamera-still --tuning-file /home/pi/bin/imx477.json --framerate 0 -o $PIC_DIR$TODAY/$TODAY$HM.jpg --metering average --exposure long -q 30 -t 20000 --post-process-file /home/pi/bin/drc.json
-mogrify -rotate 270 $PIC_DIR$TODAY/$TODAY$HM.jpg
-mogrify -pointsize 100 -fill white -undercolor '#00000080' -gravity North -annotate +0+5 "`date`" $PIC_DIR$TODAY/$TODAY$HM.jpg
-
+if ! command -v libcamera-still &> /dev/null; then
+	# For older versions of R Pi OS up to Buster
+	raspistill -o $PIC_DIR$TODAY/$TODAY$HM.jpg -a 12 -a "%Y-%m-%d %X %Z" -ae 128 -q 30 -rot 270 -ex verylong -t 20000
+else
+	# For R Pi OS Bullseye and beyond
+	timeout -s 2 60s libcamera-still --tuning-file /home/pi/bin/imx477.json --framerate 0 -o $PIC_DIR$TODAY/$TODAY$HM.jpg --metering average --exposure long -q 30 -t 20000 --post-process-file /home/pi/bin/drc.json
+	mogrify -rotate 270 $PIC_DIR$TODAY/$TODAY$HM.jpg
+	mogrify -pointsize 100 -fill white -undercolor '#00000080' -gravity North -annotate +0+5 "`date`" $PIC_DIR$TODAY/$TODAY$HM.jpg
+fi
