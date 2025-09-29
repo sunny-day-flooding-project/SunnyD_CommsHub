dc7b1aa33d266e79bfc57a838c159d0d1f042965
whipple632 <whipple@email.unc.edu>
2025-09-29 16:00:49 -0400

    Speed up

--- focus.sh
+++ focus.sh
@@ -5,10 +5,8 @@
 # Start libcamera-hello and process its output
 libcamera-hello -t 0 --info-text "Focus measure: %focus" --nopreview 2>&1 >/dev/null |
 grep --line-buffered "Focus measure" |
-sed -u 's/.*: //' |
 while read -r val; do
-    printf "\rFocus: %s" "$val"
-    sleep 0.5
+    printf "\r%s" "$val"
 done &
 pid=$!
 
